4b51cff6ebc9aac6ea1f6f0fe76cbb5b30c10710
jeanP-tech <jeanP.tech@gmail.com>
2020-03-04 18:59:57 +0900

    Fix go_detail issue

--- generate_show_none.js
+++ generate_show_none.js
@@ -3,20 +3,27 @@ const container = document.getElementById("container");
 const BOOK_NUMBER = 30;
 
 // readList
+const book = document.getElementsByClassName("book");
+const bookInfo = document.getElementsByClassName("book_info");
+const coverImg = document.getElementsByClassName("cover_img");
+const bookTitle = document.getElementsByClassName("book_title");
+const bookRank = document.getElementsByClassName("book_rank");
+const review = document.getElementsByClassName("book_review");
+
 function generateReadList () {
 
   const readList = document.createElement("div");
-  readList.id = 'read_list';
+  readList.id = "read_list";
   container.appendChild(readList);
 
   for (let i = 0; i < BOOK_NUMBER; i++) {
 
     const book = document.createElement("div");
-    book.className = 'book';
+    book.className = "book";
     readList.appendChild(book);
 
     const coverImg = new Image();
-    coverImg.src = 'images/voice.jpg';
+    coverImg.src = "images/voice.jpg";
     coverImg.className = "cover_img";
     book.appendChild(coverImg);
 
@@ -26,15 +33,15 @@ function generateReadList () {
 
     const bookTitle = document.createElement("li");
     bookTitle.className = "book_title";
-    bookTitle.innerHTML = '목소리를 드릴게요';
+    bookTitle.innerHTML = "목소리를 드릴게요";
     bookInfo.appendChild(bookTitle);
     const bookRank = document.createElement("li");
     bookRank.className = "book_rank";
-    bookRank.innerHTML = '🌟🌟🌟🌟';
+    bookRank.innerHTML = "🌟🌟🌟🌟";
     bookInfo.appendChild(bookRank);
     const review = document.createElement("li");
     review.className = "book_review";
-    review.innerHTML = '정세랑 작가의 신작';
+    review.innerHTML = "정세랑 작가의 신작";
     bookInfo.appendChild(review);
   }
 
@@ -50,14 +57,8 @@ function generateReadList () {
 
 };
 
-const book = document.getElementsByClassName("book");
-const bookInfo = document.getElementsByClassName("book_info");
-const coverImg = document.getElementsByClassName("cover_img");
-const bookTitle = document.getElementsByClassName("book_title");
-const bookRank = document.getElementsByClassName("book_rank");
-const review = document.getElementsByClassName("book_review");
-
 function showReadList () {
+
   document.getElementById("read_list").style.display = "flex";
 
   for (let i = 0; i < BOOK_NUMBER; i++){
@@ -70,6 +71,7 @@ function showReadList () {
       review[i].style.display = "block";
     }
   }
+
 }
 
 
@@ -91,17 +93,17 @@ function noneReadList() {
 function generateWishList() {
 
   const wishList = document.createElement("div");
-  wishList.id = 'wish_list';
+  wishList.id = "wish_list";
   container.appendChild(wishList);
 
   for (let i = 0; i < BOOK_NUMBER; i++) {
 
     const wishBook = document.createElement("div");
-    wishBook.className = 'wish_book';
+    wishBook.className = "wish_book";
     wishList.appendChild(wishBook);
 
     const wishCoverImg = new Image();
-    wishCoverImg.src = 'images/speed.jpg';
+    wishCoverImg.src = "images/speed.jpg";
     wishCoverImg.className = "wish_cover_img";
     wishBook.appendChild(wishCoverImg);
 
@@ -111,11 +113,11 @@ function generateWishList() {
 
     const wishBookTitle = document.createElement("li");
     wishBookTitle.className = "wish_book_title";
-    wishBookTitle.innerHTML = '우리가 빛의 속도로 갈 수 없다면';
+    wishBookTitle.innerHTML = "우리가 빛의 속도로 갈 수 없다면";
     wishBookInfo.appendChild(wishBookTitle);
     const wishBookAuthor = document.createElement("li");
     wishBookAuthor.className = "wish_book_author";
-    wishBookAuthor.innerHTML = '김초엽';
+    wishBookAuthor.innerHTML = "김초엽";
     wishBookInfo.appendChild(wishBookAuthor);
   }
 
@@ -172,17 +174,17 @@ function noneWishList() {
 function generateThrowList() {
 
   const throwList = document.createElement("div");
-  throwList.id = 'throw_list';
+  throwList.id = "throw_list";
   container.appendChild(throwList);
 
   for (let i = 0; i < BOOK_NUMBER; i++) {
 
     const throwBook = document.createElement("div");
-    throwBook.className = 'throw_book';
+    throwBook.className = "throw_book";
     throwList.appendChild(throwBook);
 
     const throwCoverImg = new Image();
-    throwCoverImg.src = 'images/work.jpg';
+    throwCoverImg.src = "images/work.jpg";
     throwCoverImg.className = "throw_cover_img";
     throwBook.appendChild(throwCoverImg);
 
@@ -192,11 +194,11 @@ function generateThrowList() {
 
     const throwBookTitle = document.createElement("li");
     throwBookTitle.className = "throw_book_title";
-    throwBookTitle.innerHTML = '일의 기쁨과 슬픔';
+    throwBookTitle.innerHTML = "일의 기쁨과 슬픔";
     throwBookInfo.appendChild(throwBookTitle);
     const throwBookAuthor = document.createElement("li");
     throwBookAuthor.className = "throw_book_author";
-    throwBookAuthor.innerHTML = '장류진';
+    throwBookAuthor.innerHTML = "장류진";
     throwBookInfo.appendChild(throwBookAuthor);
   }
 
